0a746358d14d977e75a74b2cc35be9802db5dbb1
Sergey Komarov <anaxronik@gmail.com>
2021-02-23 22:17:29 +0500

    connect webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -33,4 +34,10 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+  },
 }
